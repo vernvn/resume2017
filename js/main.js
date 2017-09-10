7c4178f75117322513abd6e5cded15a4307d767a
vernvn <996685777@qq.com>
2017-09-10 19:39:44 +0800

    feat: 'change resume'

--- js/main.js
+++ js/main.js
@@ -160,72 +160,38 @@ jQuery(document).ready(function($) {
 
     // Needed variables
     var $contactform = $('#contactform'),
-            $success = ' Your message has been sent. Thank you!';
-
-    $contactform.submit(function() {
+            $thankinfo = $('.thanke_info'),
+            response,
+            $success = ' Your message has been sent. Thank you!',
+            Ticket = "7de2841de198db4f2fdd27f2da55b8d3742d2214",
+            headers = {"Authorization": "7de2841de198db4f2fdd27f2da55b8d3742d2214"};
+    $contactform.submit(function() {  
         $.ajax({
             type: "POST",
-            url: "php/contact.php",
+            url: `https://api.github.com/users/${$contactform.serializeArray()[0].value}`,
+            headers: headers,
             data: $(this).serialize(),
+            dataType:'JSONP',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("token", Ticket);
+            },
             success: function(msg)
             {
-                var msg_error = msg.split(",");
-                var output_error = '';
-
-                if (msg_error.indexOf('error-message') != -1) {
-                    $("#contact-message").addClass("has-error");
-                    $("#contact-message").removeClass("has-success");
-                    output_error = 'Please enter your message.';
-                } else {
-                    $("#contact-message").addClass("has-success");
-                    $("#contact-message").removeClass("has-error");
+                if(!msg.data.id){
+                    return;
                 }
+                response = `<div class="alert alert-success success-send">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <i class="glyphicon glyphicon-ok"></i>   
+                                <p style="text-align: center">${msg.data.name || ''}</p> 
+                                <img  style="margin-left: 80px; width: 80px; border-radius: 50%;" src=${msg.data.avatar_url}>
+                                <p style="text-align: center">${msg.data.bio || ''}</p>  
+                                <p style="text-align: center">${msg.data.company || ''}</p>
+                                <p style="text-align: center">${msg.data.email || ''}</p>
+                                <p style="text-align: center">${msg.data.created_at || ''}</p> 
+                            </div>`
+                $thankinfo.append(response);
 
-                if (msg_error.indexOf('error-email') != -1) {
-
-                    $("#contact-email").addClass("has-error");
-                    $("#contact-email").removeClass("has-success");
-                    output_error = 'Please enter valid e-mail.';
-                } else {
-                    $("#contact-email").addClass("has-success");
-                    $("#contact-email").removeClass("has-error");
-                }
-
-                if (msg_error.indexOf('error-name') != -1) {
-                    $("#contact-name").addClass("has-error");
-                    $("#contact-name").removeClass("has-success");
-                    output_error = 'Please enter your name.';
-                } else {
-                    $("#contact-name").addClass("has-success");
-                    $("#contact-name").removeClass("has-error");
-                }
-
-
-                if (msg == 'success') {
-
-                    response = '<div class="alert alert-success success-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
-                            + '</div>';
-
-
-                    $(".reset").trigger('click');
-                    $("#contact-name").removeClass("has-success");
-                    $("#contact-email").removeClass("has-success");
-                    $("#contact-message").removeClass("has-success");
-
-                } else {
-
-                    response = '<div class="alert alert-danger error-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
-                            + '</div>';
-
-                }
-                // Hide any previous response text
-                $(".error-send,.success-send").remove();
-                // Show response message
-                $contactform.prepend(response);
             }
         });
         return false;
@@ -289,25 +255,8 @@ jQuery(document).ready(function($) {
     /* ---------------------------------------------------------------------- */
     /* ------------------------------ Google Maps --------------------------- */
     /* ---------------------------------------------------------------------- */
-
-    var map;
     function initialize() {
-        map = new GMaps({
-            div: '#map',
-            lat: -37.817917,
-            lng: 144.965065,
-            zoom: 16
-
-        });
-        map.addMarker({
-            lat: -37.81792,
-            lng: 144.96506,
-            title: 'Marker with InfoWindow',
-            icon: 'images/pins-map/map-marker.png',
-            infoWindow: {
-                content: '<p>Melbourne Victoria, 300, Australia</p>'
-            }
-        });
+        
     }
 
     /* ---------------------------------------------------------------------- */
